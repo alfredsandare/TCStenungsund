aa7c3828045b208ee863b98018d9ab5d8030bf89
Alexander Salomonsson <alexander.salomonsson02@skola.stenungsund.se>
2023-11-23 08:55:12 +0100

    commit
    serviceWorker is created and ready. I have tested it and everything works perfectly. the page works offline..
    
    manifest filen created.

---
+++ script.js
@@ -0,0 +1,13 @@
+if('serviceWorker' in navigator) {
+  console.log('Service Worker Supported');
+  window.addEventListener('load', ()=>{
+      navigator.serviceWorker
+      .register('../sw_cached_pages.js')
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(err => console.log('Service Worker Registration Failed'))
+  })
+
+} else {
+  console.log('Service Worker is not supported');
+}
+
---
+++ sw_cached_pages.js
@@ -0,0 +1,32 @@
+const cacheName = 'V1'; //skapa en "box för cacheing"
+const cacheAssets = ['index.html', '/script.js']; // infoga filerna man vill chahe
+
+self.addEventListener('install', e => { // börja med installera efter man har registerat sw
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log('Service Worker caching files');
+            cache.addAll(cacheAssets)
+        }).then(() => self.skipWaiting()) 
+    ) 
+})
+
+//activated event
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName) {
+                        return caches.delete(cache)
+                    }
+                })
+            )
+        })
+    )
+})
+
+//fetch
+self.addEventListener('fetch', e => {
+    console.log('Fetching');
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+})
